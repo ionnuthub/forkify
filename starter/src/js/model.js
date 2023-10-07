e69e3adef05021e7e79bddb63b0b505e3bd9193d
ionnuthub <raducugionut@gmail.com>
2023-10-07 16:22:51 +0200

    config -files

--- starter/src/js/model.js
+++ starter/src/js/model.js
@@ -1,3 +1,7 @@
+import { async } from 'regenerator-runtime';
+import { API_URL } from './config.js';
+import { getJSON } from './helpers.js';
+
 // Module model.js
 // State : - stores all the data about the application that is running in the browser(the data about applications front-end)
 //        - should store any data that we might fetch from an API or data that the user inputs or what page the user currently viewing
@@ -7,9 +11,31 @@
 // We keep it simple here and use a simple obj to store our entire state
 // We will start with a big state object:
 
-// We exporty the state to use it in the controller
+// We export the state to use it in the controller
 export const state = {
   recipe: {},
 };
 
-//  function responsible with fetching the data from our forkify API
+//  function responsible with fetching the data from our forkify API:
+// This fucntion will not return anything . It will just change our state object
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJSON(`${API_URL}/${id}`);
+
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    // Temp error handlig
+    console.error(`${err}💥💥💥`);
+  }
+};
