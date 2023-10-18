4675b86305ede6359fa4a4297411e20557817978
ionnuthub <raducugionut@gmail.com>
2023-10-18 16:13:26 +0200

    finish

--- dist/index.1172ed67.js
+++
@@ -1,5 +0,0 @@
-document.querySelector(".recipe");// https://forkify-api.herokuapp.com/v2
-///////////////////////////////////////
-const c=async function(){try{let c=await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"),e=await c.json();console.log(c,e)}catch(c){alert(c)}};c();//# sourceMappingURL=index.1172ed67.js.map
-
-//# sourceMappingURL=index.1172ed67.js.map
--- starter/src/js/config.js
+++ starter/src/js/config.js
@@ -1,3 +1,6 @@
 //  All the variables which should be constants and reused
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '<YOUR_KEY>';
+export const MODAL_CLOSE_SEC = 2.5;
